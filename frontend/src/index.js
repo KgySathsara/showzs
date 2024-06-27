ae7fc29345063cf0749c3038830a5c31f99738c5
Gayan Sachintha <118958174+Gayan-Sachintha@users.noreply.github.com>
2024-06-27 11:42:39 +0530

    chore: Add navigation links to Navbar component created the Liveevents.jsx

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import App from './App';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SeatBooking from './pages/seatBooking';
+import LiveEvents from './pages/LiveEvents/LiveEvents';
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App/>}/>
             <Route path='/seatBooking' element={<SeatBooking />}/>
+            <Route path='/LiveEvents' element={<LiveEvents />}/>
         </Routes>
     </BrowserRouter>,
     document.getElementById('root')
---
+++ frontend/src/pages/LiveEvents/LiveEvents.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Footer, Header, Events } from '../../containers';
+import { Navbar } from '../../components';
+// import './LiveEvents.css';
+
+const LiveEvents = () => {
+  return (
+    <div className='LiveEvents'>
+      <div className='gradient_bg'>
+        <Navbar />
+      </div>
+      <div className='live-events-content'>
+        <Events />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default LiveEvents;
