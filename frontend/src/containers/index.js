9c4ee6db67b67db9e659f4488ae3e4ab68a0cfad
Gayan Sachintha <118958174+Gayan-Sachintha@users.noreply.github.com>
2024-06-27 15:54:41 +0530

    chore: Remove unused dependencies and update package.json

--- frontend/src/containers/index.js
+++ frontend/src/containers/index.js
@@ -2,3 +2,5 @@ export { default as Footer } from './footer/Footer';
 export { default as Stream } from './stream/Stream';
 export { default as Header } from './header/Header';
 export { default as Events } from './events/Events';
+export { default as MovieDetails } from './movieDetails/MovieDetails';
+export { default as Payment } from './payment/Payment';
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SeatBooking from './pages/seatBooking';
 import LiveEvents from './pages/LiveEvents/LiveEvents';
+import Movies from './pages/Movies/Movies';
 
 ReactDOM.render(
     <BrowserRouter>
@@ -12,6 +13,8 @@ ReactDOM.render(
             <Route path='/' element={<App/>}/>
             <Route path='/seatBooking' element={<SeatBooking />}/>
             <Route path='/LiveEvents' element={<LiveEvents />}/>
+            <Route path='/Movies' element={<Movies />}/>
+
         </Routes>
     </BrowserRouter>,
     document.getElementById('root')
