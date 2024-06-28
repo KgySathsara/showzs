0ee4761e7f914eadeb9ba13d0cd90786ecb3ea34
Akith Nimpura <mpakithn@gmail.com>
2024-06-28 16:20:32 +0530

    created adminnavbar dashboard

--- frontend/src/components/navbar/Adminnavbar.jsx
+++ frontend/src/components/navbar/Adminnavbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
 import { 
+  DashboardOutlined,
   VideoCameraOutlined,
   CalendarOutlined,
   NotificationOutlined,
@@ -20,66 +21,73 @@ const AdminNavBar = () => {
         <img src={logo} alt='logo'/>
         </div>
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <SubMenu key="sub1" icon={<VideoCameraOutlined />} title="Movie Management">
-            <SubMenu key="sub2" title="Movies List">
-              <Menu.Item key="1">View Movies</Menu.Item>
-              <Menu.Item key="2">Search/Filter Movies</Menu.Item>
-              <Menu.Item key="3">Add New Movie</Menu.Item>
-              <Menu.Item key="4">Edit Movie Details</Menu.Item>
-              <Menu.Item key="5">Delete Movie</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub3" title="Categories">
-              <Menu.Item key="6">View All Categories</Menu.Item>
-              <Menu.Item key="7">Add New Category</Menu.Item>
-              <Menu.Item key="8">Edit Category</Menu.Item>
-              <Menu.Item key="9">Delete Category</Menu.Item>
+        <SubMenu key="sub1" icon={<DashboardOutlined />} title="Dashboard">
+            <SubMenu key="sub2" title="Overview">
+              <Menu.Item key="1">View Ongoing Movies</Menu.Item>
+              <Menu.Item key="2">Total Live Events</Menu.Item>
+              <Menu.Item key="3">Total News Articles</Menu.Item>
+              <Menu.Item key="4">Total Users</Menu.Item>
+              <Menu.Item key="5">Recent Activity</Menu.Item>
+              <Menu.Item key="6">User Engagement</Menu.Item>
+              <Menu.Item key="7">Movie Views</Menu.Item>
+              <Menu.Item key="8">Event Participation</Menu.Item>
+              <Menu.Item key="9">News Readership</Menu.Item>
             </SubMenu>
           </SubMenu>
 
-          <SubMenu key="sub4" icon={<CalendarOutlined />} title="Live Events Management">
-            <SubMenu key="sub5" title="Events List">
-              <Menu.Item key="10">View Event</Menu.Item>
-              <Menu.Item key="11">Search/Filter Events</Menu.Item>
-              <Menu.Item key="12">Add New Event</Menu.Item>
-              <Menu.Item key="13">Edit Event Details</Menu.Item>
-              <Menu.Item key="14">Delete Event</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub6" title="Categories">
-              <Menu.Item key="15">View All Categories</Menu.Item>
-              <Menu.Item key="16">Add New Category</Menu.Item>
-              <Menu.Item key="17">Edit Category</Menu.Item>
-              <Menu.Item key="18">Delete Category</Menu.Item>
+          <SubMenu key="sub3" icon={<VideoCameraOutlined />} title="Movie Management">
+            <SubMenu key="sub4" title="Movies List">
+              <Menu.Item key="10">View Movies</Menu.Item>
+              <Menu.Item key="11">Add New Movie</Menu.Item>
+              <Menu.Item key="12">Edit Movie Details</Menu.Item>
+              <Menu.Item key="13">Delete Movie</Menu.Item>
             </SubMenu>
           </SubMenu>
 
-          <SubMenu key="sub7" icon={<NotificationOutlined />} title="News Management">
-            <SubMenu key="sub8" title="Movie News List">
-              <Menu.Item key="19">View All Articles</Menu.Item>
-              <Menu.Item key="20">Search/Filter Articles</Menu.Item>
-              <Menu.Item key="21">Add New Article</Menu.Item>
-              <Menu.Item key="22">Edit Article</Menu.Item>
-              <Menu.Item key="23">Delete Article</Menu.Item>
+          <SubMenu key="sub5" icon={<CalendarOutlined />} title="Live Events Management">
+            <SubMenu key="sub6" title="Events List">
+              <Menu.Item key="14">View Event</Menu.Item>
+              <Menu.Item key="15">Search/Filter Events</Menu.Item>
+              <Menu.Item key="16">Add New Event</Menu.Item>
+              <Menu.Item key="17">Edit Event Details</Menu.Item>
+              <Menu.Item key="18">Delete Event</Menu.Item>
             </SubMenu>
-            <SubMenu key="sub9" title="Categories">
-              <Menu.Item key="24">View All Categories</Menu.Item>
-              <Menu.Item key="25">Add New Category</Menu.Item>
-              <Menu.Item key="26">Edit Category</Menu.Item>
-              <Menu.Item key="27">Delete Category</Menu.Item>
+            <SubMenu key="sub7" title="Categories">
+              <Menu.Item key="19">View All Categories</Menu.Item>
+              <Menu.Item key="20">Add New Category</Menu.Item>
+              <Menu.Item key="21">Edit Category</Menu.Item>
+              <Menu.Item key="22">Delete Category</Menu.Item>
             </SubMenu>
           </SubMenu>
 
-          <SubMenu key="sub10" icon={<MailOutlined />} title="Contact Us">
-            <SubMenu key="sub11" title="Messages">
-              <Menu.Item key="28">View All Messages</Menu.Item>
-              <Menu.Item key="29">Search/Filter Messages</Menu.Item>
-              <Menu.Item key="30">Respond to Message</Menu.Item>
-              <Menu.Item key="31">Delete Message</Menu.Item>
+          <SubMenu key="sub8" icon={<NotificationOutlined />} title="News Management">
+            <SubMenu key="sub9" title="Movie News List">
+              <Menu.Item key="23">View All Articles</Menu.Item>
+              <Menu.Item key="24">Search/Filter Articles</Menu.Item>
+              <Menu.Item key="25">Add New Article</Menu.Item>
+              <Menu.Item key="26">Edit Article</Menu.Item>
+              <Menu.Item key="27">Delete Article</Menu.Item>
             </SubMenu>
-            <SubMenu key="sub12" title="FAQs">
-              <Menu.Item key="32">View All FAQs</Menu.Item>
-              <Menu.Item key="33">Add New FAQ</Menu.Item>
-              <Menu.Item key="34">Edit FAQ</Menu.Item>
-              <Menu.Item key="35">Delete FAQ</Menu.Item>
+            <SubMenu key="sub10" title="Categories">
+              <Menu.Item key="28">View All Categories</Menu.Item>
+              <Menu.Item key="29">Add New Category</Menu.Item>
+              <Menu.Item key="30">Edit Category</Menu.Item>
+              <Menu.Item key="31">Delete Category</Menu.Item>
+            </SubMenu>
+          </SubMenu>
+
+          <SubMenu key="sub11" icon={<MailOutlined />} title="Contact Us">
+            <SubMenu key="sub12" title="Messages">
+              <Menu.Item key="32">View All Messages</Menu.Item>
+              <Menu.Item key="33">Search/Filter Messages</Menu.Item>
+              <Menu.Item key="34">Respond to Message</Menu.Item>
+              <Menu.Item key="35">Delete Message</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub13" title="FAQs">
+              <Menu.Item key="36">View All FAQs</Menu.Item>
+              <Menu.Item key="37">Add New FAQ</Menu.Item>
+              <Menu.Item key="38">Edit FAQ</Menu.Item>
+              <Menu.Item key="39">Delete FAQ</Menu.Item>
             </SubMenu>
           </SubMenu>
         </Menu>
