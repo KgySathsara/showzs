515394b74d95d32de9c070d56a9dac4362ce5a53
Akith Nimpura <mpakithn@gmail.com>
2024-07-03 17:00:39 +0530

    Updated News Page

--- frontend/src/containers/index.js
+++ frontend/src/containers/index.js
@@ -20,5 +20,6 @@ export { default as LiveEventPayment } from './payment/LiveEventPayment';
 export { default as MoviePayment } from './payment/MoviePayment';
 export { default as AdminUsersManagement } from './usersManagement/AdminUsersManagement';
 export { default as AdminAddNewsManagement } from './adminAddNews/AdminAddNewsManagement';
+export { default as Trailers } from './Trailers/Trailers';
 
 
