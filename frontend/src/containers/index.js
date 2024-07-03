6b123ae5abb10b85f24e4695e0f8bc39d30f040b
KgySathsara <88894196+KgySathsara@users.noreply.github.com>
2024-07-03 12:26:32 +0530

    create the Live Stream Management

--- frontend/src/containers/index.js
+++ frontend/src/containers/index.js
@@ -15,5 +15,5 @@ export { default as AdminContact } from './adminContactView/adminContactView';
 export { default as AdminUpcomingMovies } from './AdminUpcomingMovies/AdminUpcomingMovies';
 export { default as MovieProfileManagement } from './movieManagement/MovieProfileManagement';
 export { default as DashboardContainer } from './DashboardContainer/DashboardContainer';
-
+export { default as LiveStreamManagement } from './LiveStreamManagement/LiveStreamManagement';
 
