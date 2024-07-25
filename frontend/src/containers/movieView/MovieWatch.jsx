013859c1255f4f87cca0ef8bff927a87aff587fc
Akith Nimpura <mpakithn@gmail.com>
2024-07-25 12:24:18 +0530

    Added React Player to the Movie Watch & Live Watch Pages

--- frontend/src/containers/movieView/MovieWatch.jsx
+++ frontend/src/containers/movieView/MovieWatch.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './moviewatch.css';
-import sinhabahufilm from '../../assest/sinhabahuTrailer.mp4';
+import ReactPlayer from 'react-player'
 
 const MovieWatch = () => {
-    return (
-        <section className='movie-watch'>
-          <h2>Watch Movie</h2>
-          <div className='movie-watch-container'>
-            <video controls src={sinhabahufilm} alt="Sinhabahu Trailer" />
-          </div>
-        </section>
-      );
+  return (
+    <section className='movie-watch'>
+      <h2>Watch Movie</h2>
+      <div className='movie-watch-container'>
+        <ReactPlayer url='https://www.youtube.com/watch?v=28oPwzIbAyY' />
+      </div>
+    </section>
+  );
 }
 
 export default MovieWatch
