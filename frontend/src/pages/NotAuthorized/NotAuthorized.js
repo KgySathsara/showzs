d6ec3cf6bb90f85274904fca1e73268574369425
kgysathsara <yohansathsara87@gmail.com>
2024-08-06 17:53:10 +0530

    access create

---
+++ frontend/src/pages/NotAuthorized/NotAuthorized.js
@@ -0,0 +1,13 @@
+// src/pages/NotAuthorized/NotAuthorized.js
+import React from 'react';
+import Login from '../login/Login';
+
+const NotAuthorized = () => {
+  return (
+    <div>
+        <Login />
+    </div>
+  );
+};
+
+export default NotAuthorized;
